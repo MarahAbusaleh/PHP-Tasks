8d8dae9c0309eb5eb4785f423b5cbd97b6fa8de3
MarahAbusaleh <marah.abusaleh12@gmail.com>
2023-08-13 22:43:21 +0300

    I solved my today's Task

--- Tasks_13-AUG-2023/insert.php
+++ Tasks_13-AUG-2023/insert.php
@@ -12,13 +12,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql = "INSERT INTO students (student_name, age, student_email) VALUES ('$name', '$age', '$email')";
 
 
-if (mysqli_query($conn, $sql)) { 
-    // echo "<h1>New Record Inserted successfully!</h1><br>";
-    header("Location: select.php"); // Redirect to view.php
-    exit(); // Make sure to exit after redirection
-} else {
-    echo "Error: " . $stmt->error;
-}
+    if (mysqli_query($conn, $sql)) { 
+        // echo "<h1>New Record Inserted successfully!</h1><br>";
+        header("Location: select.php"); // Redirect to view.php
+        exit(); // Make sure to exit after redirection
+    } else {
+        echo "Error: " . $stmt->error;
+    }
 }
 ?>
 
